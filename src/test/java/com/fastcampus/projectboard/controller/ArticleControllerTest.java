595d7f38dfbc5b5d622131c24228220eb28ff8e7
nick <nicklee@sweetspot.co.kr>
2024-04-09 18:01:58 +0900

    #17 - 게시판 뷰 기본 골격 작성
    컨트롤러 핸드러 메소드로 테스트를 통과하게 열러줌
    뷰 적절히 뼈대 잡음
    테스트에서는 media type 검사해서 부가적인 옵션이
    들어갈 수 있음을 확인했기 때문에
    적절히 테스트 메소드 수정

--- src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -20,7 +20,6 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
@@ -29,10 +28,9 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
-                .andExpect(model().attributeExists("articles"))
-                .andExpect(model().attributeExists("articleComments"));
+                .andExpect(model().attributeExists("articles"));
     }
 
     @Disabled("구현 중")
@@ -44,7 +42,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"));
     }
