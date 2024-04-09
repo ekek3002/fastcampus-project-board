c9279fdc9ef7025abbb26ee0ed800b7d395c4d48
nick <nicklee@sweetspot.co.kr>
2024-04-09 13:47:18 +0900

    #2 - Spring Data REST 테스트 제외 처리
    이것은 이프로젝트의 비즈니스 로직으로 구현한 내용이 아니라 data rest 기능이고 통합테스트라 무거우며 db 에도 영향을 준다.
    공부 목적으로도 의미는 있기 때문에 삭제하지는 않고
    제외 처리해서 전체 테스트 중에 실행되지 않게끔 처리

--- src/test/java/com/fastcampus/projectboard/controller/DataRestTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/DataRestTest.java
@@ -1,5 +1,6 @@
 package com.fastcampus.projectboard.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Disabled("Spring Data REST 통합테스트는 불필요하므로 제외시킴")
 @DisplayName("Data REST - API 테스트")
 @Transactional
 @AutoConfigureMockMvc
@@ -54,7 +56,7 @@ public class DataRestTest {
         // Given
 
         // When & Then
-        mvc.perform(get("/api/articlesComments"))
+        mvc.perform(get("/api/articleComments"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
     }
@@ -65,7 +67,7 @@ public class DataRestTest {
         // Given
 
         // When & Then
-        mvc.perform(get("/api/articlesComments/1"))
+        mvc.perform(get("/api/articleComments/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
     }
