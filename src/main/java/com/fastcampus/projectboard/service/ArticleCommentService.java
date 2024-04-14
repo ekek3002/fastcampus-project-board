0deed6bb9c2e2ba9a10a65f1514397e47a896911
nick <nicklee@sweetspot.co.kr>
2024-04-14 12:50:08 +0900

    #22 - 서비스 로직 테스트 코드 업데이트 및 DTO 재설계, 쿼리 메소드 작성
    강의에서 보여주지 않은 나머지 로직의 테스트 코드를 작성하고
    연관된 DTO 를 재설계함
    테스트가 필요로 하는 repository 쿼리 메소드도 작성

--- src/main/java/com/fastcampus/projectboard/service/ArticleCommentService.java
+++ src/main/java/com/fastcampus/projectboard/service/ArticleCommentService.java
@@ -23,6 +23,11 @@ public class ArticleCommentService {
     }
 
     public void saveArticleComment(ArticleCommentDto dto) {
+    }
 
+    public void updateArticleComment(ArticleCommentDto dto) {
+    }
+
+    public void deleteArticleComment(Long articleCommentId) {
     }
 }
